cabbc10dff91307a6e2c9d9e7721af5cc6b0da08
Aprekek <mhlshamburov@gmail.com>
2022-01-18 17:32:56 +0700

    create base toolbar

---
+++ shared/ui/src/main/java/ru/sibsutis/table/shared/ui/ToolbarDT.kt
@@ -0,0 +1,30 @@
+package ru.sibsutis.table.shared.ui
+
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+
+@Composable
+fun ToolbarDT(
+	title: String,
+	enableBackButton: Boolean = true,
+	onBackButtonClick: () -> Unit
+) {
+	TopAppBar(
+		title = { Text(text = title) },
+		navigationIcon =
+		if (enableBackButton) {
+			{
+				IconButton(onClick = onBackButtonClick) {
+					Icon(
+						painter = painterResource(id = R.drawable.back_button_icon),
+						contentDescription = null
+					)
+				}
+			}
+		} else null
+	)
+}
