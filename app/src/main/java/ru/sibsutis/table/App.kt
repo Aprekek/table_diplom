b17dada396c808066be39ab4adca587174ae5f11
Aprekek <mhlshamburov@gmail.com>
2022-01-19 21:36:21 +0700

    create provider for shared preferences

--- app/src/main/java/ru/sibsutis/table/App.kt
+++ app/src/main/java/ru/sibsutis/table/App.kt
@@ -7,6 +7,7 @@ import org.koin.core.context.startKoin
 import org.koin.core.logger.Level.ERROR
 import ru.sibsutis.network.di.networkModule
 import ru.sibsutis.table.database.di.databaseModule
+import ru.sibsutis.table.di.appModule
 import ru.sibsutis.table.preferences.di.preferencesModule
 
 class App : Application() {
@@ -18,6 +19,7 @@ class App : Application() {
 			androidContext(this@App)
 
 			modules(
+				appModule,
 				networkModule,
 				databaseModule,
 				preferencesModule,
