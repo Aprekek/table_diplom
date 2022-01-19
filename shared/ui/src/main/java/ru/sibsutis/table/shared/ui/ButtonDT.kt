afd23886f3b7dd1e3201600660d807ba307f06da
Aprekek <mhlshamburov@gmail.com>
2022-01-19 23:31:10 +0700

    fix spaces

--- shared/ui/src/main/java/ru/sibsutis/table/shared/ui/ButtonDT.kt
+++ shared/ui/src/main/java/ru/sibsutis/table/shared/ui/ButtonDT.kt
@@ -1,11 +1,8 @@
 package ru.sibsutis.table.shared.ui
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun ButtonDT(
