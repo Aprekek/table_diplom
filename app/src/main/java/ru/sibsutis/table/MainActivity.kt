a2a330616aec7be82bf6ee032b58fef8b0c419d9
Aprekek <mhlshamburov@gmail.com>
2022-01-21 18:43:02 +0700

    added compile options for annotations

--- app/src/main/java/ru/sibsutis/table/MainActivity.kt
+++ app/src/main/java/ru/sibsutis/table/MainActivity.kt
@@ -3,13 +3,9 @@ package ru.sibsutis.table
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.ExperimentalMaterialApi
-import kotlinx.coroutines.FlowPreview
 import ru.sibsutis.table.feature.groupmenu.ui.GroupMenuScreen
 import ru.sibsutis.table.navigation.GlobalController
 
-@FlowPreview
-@ExperimentalMaterialApi
 class MainActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/ru/sibsutis/table/navigation/GlobalController.kt
+++ app/src/main/java/ru/sibsutis/table/navigation/GlobalController.kt
@@ -1,14 +1,10 @@
 package ru.sibsutis.table.navigation
 
-import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
-import kotlinx.coroutines.FlowPreview
 import ru.sibsutis.table.feature.groupmenu.ui.GroupMenuScreen
 
-@FlowPreview
-@ExperimentalMaterialApi
 @Composable
 fun GlobalController(startDestination: String) {
 	val navController = rememberNavController()
