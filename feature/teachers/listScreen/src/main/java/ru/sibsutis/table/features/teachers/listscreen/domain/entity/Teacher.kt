ff20d525a95da3a0610cabcb6f19f9232ccee97d
blizpear <blizpear@gmail.com>
2022-01-21 22:41:12 +0700

    create domain

---
+++ feature/teachers/listScreen/src/main/java/ru/sibsutis/table/features/teachers/listscreen/domain/entity/Teacher.kt
@@ -0,0 +1,10 @@
+package ru.sibsutis.table.features.teachers.listscreen.domain.entity
+
+data class Teacher(
+	val name: String,
+	val phone: String,
+	val email: String,
+	val address: String,
+	val cathedra: String,
+	val photo: String
+)
---
+++ feature/teachers/listScreen/src/main/java/ru/sibsutis/table/features/teachers/listscreen/domain/repository/TeachersListRepository.kt
@@ -0,0 +1,11 @@
+package ru.sibsutis.table.features.teachers.listscreen.domain.repository
+
+import kotlinx.coroutines.flow.Flow
+import ru.sibsutis.table.features.teachers.listscreen.domain.entity.Teacher
+
+interface TeachersListRepository {
+
+	suspend fun updateLocalStorageWithRemoteData()
+
+	fun getTeachersList(): Flow<List<Teacher>>
+}
