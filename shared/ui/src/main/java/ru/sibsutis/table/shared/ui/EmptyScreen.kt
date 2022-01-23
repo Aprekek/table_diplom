dab95941d8cbfbac9e807e3e454603e214a3e4f9
blizpear <blizpear@gmail.com>
2022-01-23 20:43:08 +0700

    create 3 screens

---
+++ shared/ui/src/main/java/ru/sibsutis/table/shared/ui/EmptyScreen.kt
@@ -0,0 +1,45 @@
+package ru.sibsutis.table.shared.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun EmptyScreen(modifier: Modifier = Modifier) {
+	Column(
+		modifier = modifier
+			.fillMaxSize()
+			.padding(horizontal = 25.dp)
+			.verticalScroll(ScrollState(0)),
+		horizontalAlignment = Alignment.CenterHorizontally,
+		verticalArrangement = Arrangement.Center
+	) {
+		Image(
+			painterResource(id = R.drawable.ic_empty),
+			colorFilter = ColorFilter.tint(Color.Black),
+			contentDescription = "Error image"
+		)
+		Text(text = stringResource(id = R.string.empty))
+	}
+}
+
+@Composable
+@Preview
+private fun emptyPreview(){
+	EmptyScreen(Modifier.background(Color.White))
+}
