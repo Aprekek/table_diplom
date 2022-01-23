48d3b197a44d511f7edc94f231ac3d7824871193
Aprekek <mhlshamburov@gmail.com>
2022-01-23 21:27:30 +0700

    added navigation environment to `change group` screen

--- navigation/src/main/java/ru/sibsutis/table/navigation/di/NavigationModule.kt
+++ navigation/src/main/java/ru/sibsutis/table/navigation/di/NavigationModule.kt
@@ -2,6 +2,7 @@ package ru.sibsutis.table.navigation.di
 
 import androidx.navigation.NavController
 import org.koin.dsl.module
+import ru.sibsutis.table.navigation.screens.changegroup.ChangeGroupNavigationRouter
 import ru.sibsutis.table.navigation.screens.settings.SettingsRouter
 import ru.sibsutis.table.navigation.screens.startinggroupmenu.StartingGroupMenuRouter
 
@@ -10,4 +11,6 @@ val navigationModule = module {
 	factory { (navController: NavController) -> StartingGroupMenuRouter(navController) }
 
 	factory { (navController: NavController) -> SettingsRouter(navController) }
+
+	factory { (navController: NavController) -> ChangeGroupNavigationRouter(navController) }
 }
