9636153db591a8ffef33416156d609ad6f98162a
blizpear <blizpear@gmail.com>
2022-01-26 19:23:24 +0700

    add copy email in clipboard

--- feature/teachers/detailsScreen/src/main/java/ru/sibsutis/table/feature/teachers/detailsscreen/ui/DetailsComponent.kt
+++ feature/teachers/detailsScreen/src/main/java/ru/sibsutis/table/feature/teachers/detailsscreen/ui/DetailsComponent.kt
@@ -1,5 +1,9 @@
 package ru.sibsutis.table.feature.teachers.detailsscreen.ui
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context.CLIPBOARD_SERVICE
+import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,6 +15,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
@@ -27,9 +32,10 @@ import ru.sibsutis.table.feature.teachers.detailsscreen.presentation.DetailsStat
 
 @Composable
 fun MainContent(
-	state: DetailsState.Content,
-	onEmailClick: (String) -> Unit = {}
+	state: DetailsState.Content
 ) {
+
+	val context = LocalContext.current
 
 	ConstraintLayout(modifier = Modifier.fillMaxSize()) {
 
@@ -111,7 +117,14 @@ fun MainContent(
 				.clickable(
 					enabled = true,
 					role = Role.Button
-				) { onEmailClick(state.teacher.email) }
+				) {
+					(context.getSystemService(CLIPBOARD_SERVICE) as ClipboardManager).apply {
+						setPrimaryClip(ClipData.newPlainText(state.teacher.email, state.teacher.email))
+					}
+					Toast
+						.makeText(context, context.getString(R.string.details_copy), Toast.LENGTH_SHORT)
+						.show()
+				}
 				.constrainAs(emailContent) {
 					start.linkTo(parent.start)
 					end.linkTo(parent.end)
