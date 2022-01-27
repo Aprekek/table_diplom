7d48a080de876f22bfbe226bfb4ae536ae6f8341
blizpear <blizpear@gmail.com>
2022-01-27 23:14:12 +0700

    make savable state of RecyclerView

--- feature/teachers/listScreen/src/main/java/ru/sibsutis/table/features/teachers/listscreen/presentation/TeachersListViewModel.kt
+++ feature/teachers/listScreen/src/main/java/ru/sibsutis/table/features/teachers/listscreen/presentation/TeachersListViewModel.kt
@@ -24,6 +24,9 @@ class TeachersListViewModel(
 
 	private lateinit var router: TeacherRouter
 
+	var firstElement: Int = 0
+	var offsetElement: Int = 0
+
 	private val _state = MutableStateFlow<TeachersListState>(TeachersListState.Initialize)
 	val state = _state.asStateFlow()
 
@@ -82,6 +85,11 @@ class TeachersListViewModel(
 				_state.value = TeachersListState.Content(groupedList)
 			}
 			.launchIn(viewModelScope)
+	}
+
+	fun onLeavingComposition(firstElement: Int, offsetElement: Int) {
+		this.firstElement = firstElement
+		this.offsetElement = offsetElement
 	}
 
 	fun navigateToDetailsScreen(name: String) {
--- feature/teachers/listScreen/src/main/java/ru/sibsutis/table/features/teachers/listscreen/ui/components/TeachersList.kt
+++ feature/teachers/listScreen/src/main/java/ru/sibsutis/table/features/teachers/listscreen/ui/components/TeachersList.kt
@@ -1,30 +1,31 @@
 package ru.sibsutis.table.features.teachers.listscreen.ui.components
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Divider
-import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import ru.sibsutis.table.features.teachers.listscreen.R.string
 import ru.sibsutis.table.features.teachers.listscreen.domain.entity.Teacher
-import ru.sibsutis.table.shared.ui.ToolbarDT
 
 @ExperimentalFoundationApi
 @Composable
 fun TeachersList(
 	modifier: Modifier = Modifier,
 	teachers: Map<Char, List<Teacher>>,
-	onTeacherClick: (Teacher) -> Unit = {}
+	onTeacherClick: (Teacher) -> Unit = {},
+	firstElement: Int,
+	offsetElement: Int,
+	onLeavingComposition: (firstElement: Int, offsetElement: Int) -> Unit = { _: Int, _: Int -> }
 ) {
 
+	val listState = rememberLazyListState(firstElement, offsetElement)
+
 	LazyColumn(
-		modifier = modifier.fillMaxSize()
+		modifier = modifier.fillMaxSize(),
+		state = listState
 	) {
 
 		teachers.forEach { (header, teacherList) ->
@@ -41,29 +42,12 @@ fun TeachersList(
 			}
 		}
 	}
-}
-
-@ExperimentalFoundationApi
-@Composable
-@Preview
-private fun ListPreview() {
-	val teachers = listOf(
-		Teacher("Храмова ТАТЬЯНА ВИКТРОВНА", "89994662475", "email@email.com", "k1 aud 446", "ИВТ", "345"),
-		Teacher("Конин Максим Васильевич", "89994662475", "email@email.com", "k1 aud 446", "ИВТ", "345"),
-		Teacher("Абрамова Евгения Сергеевна", "89994662475", "email@email.com", "k1 aud 446", "ИВТ", "345"),
-		Teacher("Храмова Антонина Павловна", "89994662475", "email@email.com", "k1 aud 446", "ИВТ", "345"),
-	)
-	val groupedList = teachers.groupBy { it.name[0] }.toSortedMap()
-	Scaffold(topBar = {
-		ToolbarDT(
-			title = stringResource(id = string.teacher_title),
-			enableBackButton = false
-		)
-	}) {
-
-		TeachersList(
-			teachers = groupedList,
-			modifier = Modifier.background(Color.White)
-		)
+	DisposableEffect(true) {
+		onDispose {
+			onLeavingComposition(
+				listState.firstVisibleItemIndex,
+				listState.firstVisibleItemScrollOffset
+			)
+		}
 	}
 }
