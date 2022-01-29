7af9a78b9ed7c24b0cd1077feae60f4298b25639
Aprekek <mhlshamburov@gmail.com>
2022-01-29 16:34:37 +0700

    create timetable screen

--- feature/timetable/src/main/java/ru/sibsutis/table/feature/timetable/ui/TimetableScreen.kt
+++ feature/timetable/src/main/java/ru/sibsutis/table/feature/timetable/ui/TimetableScreen.kt
@@ -1,7 +1,24 @@
 package ru.sibsutis.table.feature.timetable.ui
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.google.accompanist.pager.rememberPagerState
+import org.koin.androidx.compose.viewModel
+import org.koin.core.parameter.parametersOf
+import ru.sibsutis.table.feature.timetable.R
+import ru.sibsutis.table.feature.timetable.presentation.TimetableState
+import ru.sibsutis.table.feature.timetable.presentation.TimetableViewModel
 import ru.sibsutis.table.navigation.screens.timetable.TimeTableContent
+import ru.sibsutis.table.shared.ui.ErrorScreen
+import ru.sibsutis.table.shared.ui.LoadingScreen
 
 object TimetableScreen : TimeTableContent {
 
@@ -11,5 +28,60 @@ object TimetableScreen : TimeTableContent {
 
 	@Composable
 	private fun Content(group: String) {
+		val context = LocalContext.current
+		val viewModel by viewModel<TimetableViewModel> {
+			parametersOf(group)
+		}
+
+		val state by viewModel.state.collectAsState()
+		val week by viewModel.week.collectAsState()
+		val day by viewModel.day.collectAsState()
+
+		val weeksNames = remember {
+			context.resources.getStringArray(R.array.weeks)
+		}
+		val dayNames = remember {
+			context.resources.getStringArray(R.array.days)
+		}
+
+		val pagerState = rememberPagerState(initialPage = day)
+
+		Scaffold(
+			topBar = {
+				TimetableAppBar(
+					week = week,
+					day = day,
+					weeksNames = weeksNames,
+					dayNames = dayNames,
+					pagerState = pagerState,
+					dates = viewModel.getDates(),
+					onWeekSelected = { viewModel.onWeekSelect(it) },
+					onDaySelected = { viewModel.onDaySelect(it) }
+				)
+			}
+		) {
+			when (state) {
+				is TimetableState.Initial,
+				is TimetableState.Loading -> LoadingScreen()
+
+				is TimetableState.Error   -> ErrorScreen { }
+
+				is TimetableState.Content -> TimetableMainContent(
+					modifier = Modifier
+						.fillMaxSize()
+						.padding(bottom = it.calculateBottomPadding()),
+					context = context,
+					pagerState = pagerState,
+					pagesCount = TimetableViewModel.TOTAL_DAYS,
+					lessons = (state as TimetableState.Content).lessons
+				)
+			}
+		}
+
+		DisposableEffect(true) {
+			onDispose {
+				viewModel.onDaySelect(pagerState.currentPage)
+			}
+		}
 	}
 }
