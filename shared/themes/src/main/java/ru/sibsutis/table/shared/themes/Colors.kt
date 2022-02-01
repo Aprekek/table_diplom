6a3f90a0e4975695d0c3f1a69e5d44a398c51ba4
Aprekek <mhlshamburov@gmail.com>
2022-02-01 19:22:19 +0700

    create themes environment

---
+++ shared/themes/src/main/java/ru/sibsutis/table/shared/themes/Colors.kt
@@ -0,0 +1,10 @@
+package ru.sibsutis.table.shared.themes
+
+import androidx.compose.ui.graphics.Color
+
+val blue700 = Color(0xff2962ff)
+val darkBlue700 = Color(0xff0039cb)
+val veryLightBlue = Color(0xffEAF0FF) // blue700.copy(alpha = 0.03f)
+val darkGray = Color(0xff121212)
+val grey1Dp = Color(0xff1E1E1E)
+val grey4Dp = Color(0xff282828)
